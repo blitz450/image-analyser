7df68c2be79da6af3dc685f17a0a0b73a34c3f20
blitz450 <saurabh.chauhan450@gmail.com>
2019-01-20 17:31:25 +0530

    changed image width

--- src/components/ImageShow/ImageShow.js
+++ src/components/ImageShow/ImageShow.js
@@ -3,9 +3,9 @@ import './ImageShow.css';
 
 const ImageShow = ({imageUrl, faceBoxes})=>{
  return(
- 	<div className='center ma'>
+ 	<div className=' center ma'>
  		<div className='absolute mt2'>
- 			<img id='inputimage' alt='' width='500' height='auto' src={imageUrl}/>
+ 			<img id='inputimage' alt='' width='800px' height='auto' src={imageUrl}/>
  			{faceBoxes.map((faceBox,i)=>{
  				return(
  						<div key={i} className='bounding-box absolute' style={{top: faceBox.topRow, right: faceBox.rightCol, bottom:faceBox.bottomRow, left: faceBox.leftCol}}></div>
