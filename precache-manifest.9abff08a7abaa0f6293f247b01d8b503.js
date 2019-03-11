bfaa39d24a2b4f23c76e1f9ad6cd63eee6ba5db7
blitz450 <saurabh.chauhan450@gmail.com>
2019-03-11 23:58:11 +0530

    Updates

---
+++ precache-manifest.9abff08a7abaa0f6293f247b01d8b503.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82f4224820254617d89870b48a8d036f",
+    "url": "/image-analyser/static/media/logo.82f42248.svg"
+  },
+  {
+    "revision": "c8be4a222b36f94d2aaf",
+    "url": "/image-analyser/static/js/runtime~main.c8be4a22.js"
+  },
+  {
+    "revision": "f26b424dd5fe63d080ce",
+    "url": "/image-analyser/static/js/main.f26b424d.chunk.js"
+  },
+  {
+    "revision": "b9874086e99a8c79aa61",
+    "url": "/image-analyser/static/js/1.b9874086.chunk.js"
+  },
+  {
+    "revision": "f26b424dd5fe63d080ce",
+    "url": "/image-analyser/static/css/main.b8cdf67b.chunk.css"
+  },
+  {
+    "revision": "b9874086e99a8c79aa61",
+    "url": "/image-analyser/static/css/1.15682c63.chunk.css"
+  },
+  {
+    "revision": "08fd7ccb1e25841e27c16a2411924477",
+    "url": "/image-analyser/index.html"
+  }
+];
